005a25836ec3f4b5e5447a0567e2a771285b09f8
junTaniguchi <ultia0602@gmail.com>
2020-01-16 17:01:41 +0900

    APIのURIにて末尾?は許可されていなかったため、削除

--- index.py
+++ index.py
@@ -5,32 +5,40 @@ Created on Thu Jan 16 14:45:40 2020
 @author: tie305403
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+#from flask_restplus import Api, Resource, fields
+
 app = Flask(__name__)
+#api = Api(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/login/?')
+@app.route('/api/login/', methods=['POST'])
 def login():
-    return "Hello World"
+    message = "Hello World"
+    return jsonify({'message': message})
 
-@app.route('/api/get/point=?')
+@app.route('/api/get/point', methods=['GET'])
 def get_point():
-    return "Hello World"
+    message = "Hello World"
+    return jsonify({'message': message})
 
-@app.route('/api/get/balance=?')
+@app.route('/api/get/balance', methods=['GET'])
 def get_balance():
-    return "Hello World"
+    message = "Hello World"
+    return jsonify({'message': message})
 
-@app.route('/api/get/details=?')
+@app.route('/api/get/details', methods=['GET'])
 def get_details():
-    return "Hello World"
+    message = "Hello World"
+    return jsonify({'message': message})
 
-@app.route('/api/post/point=?')
+@app.route('/api/post/point', methods=['GET'])
 def post_point():
-    return "Hello World"
+    message = "Hello World"
+    return jsonify({'message': message})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8888, threaded=True)
+    app.run(debug=True, port=5000, threaded=True)
