0f167483d6077c84d5a6af1be6c86a843f9adb49
junTaniguchi <ultia0602@gmail.com>
2020-01-16 16:16:55 +0900

    first commit

---
+++ index.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 16 14:45:40 2020
+
+@author: tie305403
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+@app.route('/api/login/?')
+def login():
+    return "Hello World"
+
+@app.route('/api/get/point=?')
+def get_point():
+    return "Hello World"
+
+@app.route('/api/get/balance=?')
+def get_balance():
+    return "Hello World"
+
+@app.route('/api/get/details=?')
+def get_details():
+    return "Hello World"
+
+@app.route('/api/post/point=?')
+def post_point():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8888, threaded=True)
