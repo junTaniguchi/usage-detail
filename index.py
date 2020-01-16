ab5dba8315b477c38c4355fcdf3a2c8de194f0a0
junTaniguchi <ultia0602@gmail.com>
2020-01-16 19:14:18 +0900

    コンポ以外完成

--- index.py
+++ index.py
@@ -5,11 +5,10 @@ Created on Thu Jan 16 14:45:40 2020
 @author: tie305403
 """
 
-from flask import Flask, render_template, jsonify
-#from flask_restplus import Api, Resource, fields
+from flask import Flask, render_template, request, abort, jsonify
 
 app = Flask(__name__)
-#api = Api(app)
+#api = Blueprint('api', __name__, url_prefix='/api')
 
 @app.route('/')
 def index():
@@ -29,18 +28,24 @@ def post_login():
 
     Parameters
     ----------
-    mem_id : string
+    mem_id : str
         会員を管理するユニークID。
-    password : string
+    password : str
         会員に紐づくパスワード。
 
     Returns
     -------
     auth_result : int
         認証結果。ログインが成功したかを確認するフラグ。'0'：成功 '1'：失敗
-    session_id : string
+    session_id : str
         セッションID 0faa025f
     """
+    payload = request.json
+    mem_id = payload.get('mem_id')
+    password = payload.get('password')
+
+    #if not mem_id or not password:
+    #    abort(404, {'auth_result': '1', 'session_id': null})
 
     auth_result = 0
     session_id = '0faa025f'
@@ -57,7 +62,7 @@ def get_customer(session_id=None):
 
     Parameters
     ----------
-    session_id : string
+    session_id : str
         セッションID 0faa025f
 
     Returns
@@ -67,6 +72,10 @@ def get_customer(session_id=None):
     customer_info : dict
         照会結果  ユーザの情報。指名、生年月日、住所、メールアドレス、電話番号
     """
+
+    #if not session_id:
+    #    abort(404, {'auth_result': '1', 'customer_info': null})
+
     auth_result = 0
     customer_info = {}
     return jsonify({'auth_result': auth_result, 'customer_info': customer_info})
@@ -85,7 +94,7 @@ def get_balance(session_id=None):
     
     Parameters
     ----------
-    session_id : string
+    session_id : str
         セッションID 0faa025f
 
     Returns
@@ -97,6 +106,10 @@ def get_balance(session_id=None):
     balance_info : dict
         残高情報。総枠、割賦枠、CS枠、総利用額、割賦利用額、CS利用額、総利用残高、割賦利用残高、CS利用残高
     """
+
+    #if not session_id:
+    #    abort(404, {'auth_result': '1', 'ask_result': 2, 'balance_info': null})
+
     auth_result = 0
     ask_result = 0
     balance_info = {}
@@ -118,18 +131,22 @@ def get_details(session_id=None):
     
     Parameters
     ----------
-    session_id : string
+    session_id : str
         セッションID 0faa025f
 
     Returns
     -------
     auth_result : int
-        認証結果。ログインが成功したかを確認するフラグ。'0'：成功 '1'：失敗 '2'：XX
+        認証結果。ログインが成功したかを確認するフラグ。'0'：成功 '1'：失敗
     ask_result : int
         照会結果  '0'：成功 '1'：失敗 '2'：未実行
     detail_info : dict
         利用明細。ご利用年月日、ご利用場所、ご利用金額、支払方法
     """
+
+    #if not session_id:
+    #    abort(404, {'auth_result': '1', 'ask_result': 2, 'balance_info': null})
+
     auth_result = 0
     ask_result = 0
     detail_info = {}
