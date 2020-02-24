11746a4c37179cb9028d1fc6260bbe516c8863a5
juntaniguchi <junTaniguchi@users.noreply.github.com>
2020-02-25 01:13:58 +0900

    Update index.py

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ Created on Thu Jan 16 14:45:40 2020
 """
 
 from flask import Flask, render_template, request, abort, jsonify
+import requests
 
 app = Flask(__name__)
 #api = Blueprint('api', __name__, url_prefix='/api')
@@ -17,7 +18,7 @@ session_id = ''
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "hello"
 
 @app.route('/api/login/', methods=['POST'])
 def post_login():
@@ -45,6 +46,7 @@ def post_login():
     session_id : str
         セッションID 0faa025f
     """
+    headers = {'Authorization': 'Bearer {}'.format('a')}
     payload = request.json
     mem_id = payload.get('mem_id')
     password = payload.get('password')
@@ -62,9 +64,10 @@ def post_login():
     #セッションIDをサーバ側で保持
     #session_id = req_post.json()['session_id']
     #auth_result = req_post.json()['auth_result']
+    
     session_id = '0faa025f'
     auth_result = 0
-    return jsonify({'auth_result': auth_result, 'session_id': SESSION_ID})
+    return jsonify({'auth_result': auth_result, 'session_id': session_id})
 
 @app.route('/api/get/user/<string:session_id>', methods=['GET'])
 def get_customer(session_id=None):
@@ -92,12 +95,19 @@ def get_customer(session_id=None):
     #    abort(404, {'auth_result': '1', 'customer_info': null})
 
     #API Call
-    #r_get = requests.get(URL_ITEMS + '/api/get/user/' + session_id, headers=headers)
-    #auth_result = req_post.json()['auth_result']
-    #customer_info = req_post.json()['customer_info']
+    headers = {'Authorization': 'Bearer {}'.format('a')}
+    #req_get = requests.get(URL_ITEMS + '/api/get/user/' + session_id, headers=headers)
+    #auth_result = req_get.json()['auth_result']
+    #customer_info = req_get.json()['customer_info']
 
     auth_result = 0
-    customer_info = {}
+    customer_info = {
+        'user_name' : 'TIS株式会社',
+        'birthday'  : '20080401',
+        'address'   : '東京都新宿区西新宿8丁目17番1号住友不動産新宿グランドタワー',
+        'email'     : 'XXXXX@tis.co.jp',
+        'phone'     : '03-5337-7070'
+    }
     return jsonify({'auth_result': auth_result, 'customer_info': customer_info})
 
 @app.route('/api/get/balance/<string:session_id>', methods=['GET'])
@@ -131,14 +141,89 @@ def get_balance(session_id=None):
     #    abort(404, {'auth_result': '1', 'ask_result': 2, 'balance_info': null})
 
     #API Call
-    #r_get = requests.get(URL_ITEMS + '/api/get/balance/' + session_id, headers=headers)
-    #auth_result = req_post.json()['auth_result']
-    #ask_result = req_post.json()['ask_result']
-    #balance_info = req_post.json()['balance_info']
+    #headers = {'Authorization': 'Bearer {}'.format('a')}
+    #req_get = requests.get(URL_ITEMS + '/api/get/balance/' + session_id, headers=headers)
+    #auth_result = req_get.json()['auth_result']
+    #ask_result = req_get.json()['ask_result']
+    #balance_info = req_get.json()['balance_info']
 
     auth_result = 0
     ask_result = 0
-    balance_info = {}
+    
+    balance_info = {
+        'total' : {
+            'all' : {
+                'frame'		: 1000000,
+                'usage'		: 600000,
+                'balance'	: 400000,
+            },
+            'sp' : {
+                'frame'		: 1000000,
+                'usage'		: 250000,
+                'balance'	: 300000,
+            },
+            'installment' : {
+                'frame'		: 500000,
+                'usage'		: 200000,
+                'balance'	: 300000,
+            },
+            'cs' : {
+                'frame'		: 500000,
+                'usage'		: 150000,
+                'balance'	: 350000,
+            }
+        },
+        'installment' : {
+            'all' : {
+                'frame'		: 500000,
+                'usage'		: 200000,
+                'balance'	: 300000,
+            },
+            'twice' : {
+                'frame'		: 200000,
+                'usage'		: 50000,
+                'balance'	: 150000,
+            },
+            'manytimes' : {
+                'frame'		: 200000,
+                'usage'		: 50000,
+                'balance'	: 150000,
+            },
+            'bonus' : {
+                'frame'		: 200000,
+                'usage'		: 50000,
+                'balance'	: 150000,
+            },
+            'revolving' : {
+                'frame'		: 200000,
+                'usage'		: 50000,
+                'balance'	: 150000,
+            },
+        },
+        'cs' : {
+            'all' : {
+                'frame'		: 500000,
+                'usage'		: 150000,
+                'balance'	: 350000,
+            },
+            'domestic' : {
+                'frame'		: 300000,
+                'usage'		: 50000,
+                'balance'	: 300000,
+            },
+            'overseas' : {
+                'frame'		: 300000,
+                'usage'		: 50000,
+                'balance'	: 300000,
+            },
+            'revolving' : {
+                'frame'		: 300000,
+                'usage'		: 50000,
+                'balance'	: 300000,
+            },
+        },
+    }
+    
     return jsonify({'auth_result': auth_result, 'ask_result': ask_result, 'balance_info': balance_info})
 
 @app.route('/api/get/details/<string:session_id>', methods=['GET'])
@@ -174,15 +259,136 @@ def get_details(session_id=None):
     #    abort(404, {'auth_result': '1', 'ask_result': 2, 'balance_info': null})
 
     #API Call
-    #r_get = requests.get(URL_ITEMS + '/api/get/details/' + session_id, headers=headers)
-    #auth_result = req_post.json()['auth_result']
-    #ask_result = req_post.json()['ask_result']
-    #detail_info = req_post.json()['detail_info']
+    #headers = {'Authorization': 'Bearer {}'.format('a')}
+    #req_get = requests.get(URL_ITEMS + '/api/get/details/' + session_id, headers=headers)
+    #auth_result = req_get.json()['auth_result']
+    #ask_result = req_get.json()['ask_result']
+    #detail_info = req_get.json()['detail_info']
 
     auth_result = 0
     ask_result = 0
-    detail_info = {}
+    detail_info = [
+        {
+            'record_no'   : 1,
+            'usage_date'  : '20080401',
+            'usage_shop'  : 'd社',
+            'usage_amount': 100,
+            'method'      : 'card'
+        },
+        {
+            'record_no'   : 2,
+            'usage_date'  : '20080401',
+            'usage_shop'  : 'k社',
+            'usage_amount': 1,
+            'method'      : 'card'
+        }
+    ]
     return jsonify({'auth_result': auth_result, 'ask_result': ask_result, 'detail_info': detail_info})
+
+@app.route('/api/get/chart/<string:session_id>', methods=['GET'])
+def get_chart(session_id=None):
+    """
+    ご利用明細照会
+
+    APIコール元からセッションIDとページ数を受領し、データベース上のセッション情報と照合する。
+    セッションIDによる認証に成功した場合は、認証結果'0'を設定し、セッションIDに紐づく会員番号でご利用明細情報を検索する。
+    セッションIDによる認証に失敗した場合は、認証結果'1'と照会結果'2'を設定し、データを返却する。
+
+    ご利用明細情報の検索に成功した場合は、照会結果'0'とご利用明細情報（0～1の項目）を設定する。
+    引数のページ数が'01'のときのご利用明細情報は、ご利用年月日の降順でソートして1～10件目を設定する。
+    引数のページ数に応じて、設定するご利用明細書情報の対象が10件単位で変化する。
+    ご利用明細が存在しない（カード利用がない）場合は、ご利用明細情報は設定しない。
+    
+    Parameters
+    ----------
+    session_id : str
+        セッションID 0faa025f
+
+    Returns
+    -------
+    auth_result : int
+        認証結果。ログインが成功したかを確認するフラグ。'0'：成功 '1'：失敗
+    ask_result : int
+        照会結果  '0'：成功 '1'：失敗 '2'：未実行
+    detail_info : dict
+        利用明細。ご利用年月日、ご利用場所、ご利用金額、支払方法
+    """
+
+    #if not session_id:
+    #    abort(404, {'auth_result': '1', 'ask_result': 2, 'balance_info': null})
+
+    #API Call
+    #headers = {'Authorization': 'Bearer {}'.format('a')}
+    #req_get = requests.get(URL_ITEMS + '/api/get/details/' + session_id, headers=headers)
+    #auth_result = req_get.json()['auth_result']
+    #ask_result = req_get.json()['ask_result']
+    #detail_info = req_get.json()['detail_info']
+
+    auth_result = 0
+    ask_result = 0
+    chart_data = {
+        '外食':{
+            'スターバックスコーヒー': [
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+            ],
+            '蒙古タンメン仲本': [
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+            ]
+        },
+        '医療薬品':{
+            '市ヶ谷歯科クリニック': [
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+            ],
+            'マツモトキヨシ': [
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+            ]
+        },
+        'ホテル': {
+            'ニューオータニ': [
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+            ],
+            'HILTON': [
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+            ]
+        },
+        'エンターテイメント': {
+            'TOHOシネマズ': [
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                { '利用日':'2018/04/01', 'ご利用額': 1000 },
+            ],
+            '伊勢丹': {
+                'ルイヴィトン': [
+                    { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                    { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                    { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                ],
+                '資生堂': [
+                    { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                    { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                    { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                ],
+                '伊勢丹': [
+                    { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                    { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                    { '利用日':'2018/04/01', 'ご利用額': 1000 },
+                ]
+            }
+        }
+    };
+    return jsonify({'auth_result': auth_result, 'ask_result': ask_result, 'chart_data': chart_data})
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, threaded=True)
